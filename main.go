614d9ce29839d729f2a48297c453615509c82841
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2026-01-20 01:40:51 +0300

    developed all logic of game

--- main.go
+++ main.go
@@ -1,44 +1,58 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// ANSI escape‑коды
-const (
+var (
 	Reset = "\033[0m"
 	Red   = "\033[31m"
 	Blue  = "\033[34m"
 )
 
-func printMap(mapField [3][3]string) {
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			switch mapField[i][j] {
-			case "X":
-				fmt.Print(Red, "X", Reset)
-			case "O":
-				fmt.Print(Blue, "O", Reset)
-			default:
-				fmt.Print(mapField[i][j])
-			}
-			if j != 3 {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Println()
+func main() {
+	var mapField = [3][3]string{
+		{"1", "2", "3"},
+		{"4", "5", "6"},
+		{"7", "8", "9"},
 	}
-}
+	turn := false
+	for i := 0; i < 9; i++ {
+		// Чей ход
+		if turn == false {
+			fmt.Println("Ходят крестики")
+		} else {
+			fmt.Println("Ходят нолики")
+		}
 
-func isMoveCorrect(mapField [3][3]string, cellNumber string) bool {
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			if mapField[i][j] == cellNumber {
-				return true
+		// Текущее состояние поля
+		printMap(mapField)
+
+		// Ход игрока
+		fmt.Println("Введите цифру вашего хода:")
+		mapField = makeMove(mapField, getPlayerCellNumber(mapField), turn)
+
+		// Проверка победы
+		if hasWinner(mapField) {
+			printMap(mapField)
+			if turn == false {
+				fmt.Println("Крестики победили!")
+				return
+			} else {
+				fmt.Println("Нолики победили!")
+				return
 			}
 		}
+
+		// Изменить ход игрока
+		if turn {
+			turn = false
+		} else {
+			turn = true
+		}
 	}
-	return false
+
+	// Ничья
+	printMap(mapField)
+	fmt.Println("Ничья!")
 }
 
 func containsDigit(s string) bool {
@@ -78,6 +92,36 @@ func makeMove(mapField [3][3]string, cellNumber string, isZeroNow bool) [3][3]st
 		}
 	}
 	return mapField
+}
+
+func isMoveCorrect(mapField [3][3]string, cellNumber string) bool {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if mapField[i][j] == cellNumber {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func printMap(mapField [3][3]string) {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			switch mapField[i][j] {
+			case "X":
+				fmt.Print(Red, "X", Reset)
+			case "O":
+				fmt.Print(Blue, "O", Reset)
+			default:
+				fmt.Print(mapField[i][j])
+			}
+			if j != 3 {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func hasWinner(mapField [3][3]string) bool {
@@ -137,23 +181,13 @@ func hasWinner(mapField [3][3]string) bool {
 		return true
 	}
 
-	// diagonals check X
-	if mapField[0][0] == "X" && mapField[1][1] == "X" && mapField[2][2] == "X" {
+	// diagonals check O
+	if mapField[0][0] == "O" && mapField[1][1] == "O" && mapField[2][2] == "O" {
 		return true
 	}
-	if mapField[0][2] == "X" && mapField[1][1] == "X" && mapField[2][0] == "X" {
+	if mapField[0][2] == "O" && mapField[1][1] == "O" && mapField[2][0] == "O" {
 		return true
 	}
 
 	return false
-}
-
-func main() {
-	var mapField = [3][3]string{
-		{"1", "2", "3"},
-		{"4", "5", "6"},
-		{"7", "8", "9"},
-	}
-	printMap(mapField)
-
 }
