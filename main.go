186b96049081d2892f65cce910b106e732cfbc5d
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2026-01-20 00:38:17 +0300

    developed printMap function

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+// ANSI escape‑коды
+const (
+	Reset = "\033[0m"
+	Red   = "\033[31m"
+	Blue  = "\033[34m"
 )
 
+func printMap(mapField [3][3]string) {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			switch mapField[i][j] {
+			case "X":
+				fmt.Print(Red, "X", Reset)
+			case "O":
+				fmt.Print(Blue, "O", Reset)
+			default:
+				fmt.Print(mapField[i][j])
+			}
+			if j != 3 {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
-	fmt.Println("TicTacToe")
+	// var mapField = [3][3]string{
+	// 	{"1", "2", "3"},
+	// 	{"4", "5", "6"},
+	// 	{"7", "8", "9"},
+	// }
+	mapField := [3][3]string{
+		{"1", "2", "O"},
+		{"4", "5", "6"},
+		{"X", "8", "9"},
+	}
+	printMap(mapField)
+
 }
